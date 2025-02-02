4028d3559a5128b072e30fd5c46125ca4e42a869
kharitha999 <2003haritha@gmail.com>
2025-02-02 23:10:53 +0530

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Pokedex',
+      title: ' Pokedex ',
       theme: ThemeData(
         primarySwatch: Colors.red,
         scaffoldBackgroundColor: Colors.white,
